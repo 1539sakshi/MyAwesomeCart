403e95f231a84bbe0208f5ea91aa05837b76c172
sakshi singh <1539sakshi@gmail.com>
2020-11-29 01:07:05 +0530

    static page added

--- mac/blog/views.py
+++ mac/blog/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-  return HttpResponse("This is Index Page")
+  return render(request, 'blog/index.html')
--- mac/shop/views.py
+++ mac/shop/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-  return HttpResponse("This is Shop page")
+  return render(request, 'shop/index.html')
