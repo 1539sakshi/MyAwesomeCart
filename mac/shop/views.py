43ce37c6bb8e109fd00abf61a3138977651c6eb4
sakshi singh <1539sakshi@gmail.com>
2020-11-29 02:37:08 +0530

    some models added

--- mac/shop/views.py
+++ mac/shop/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
   return render(request, 'shop/index.html')
+
+def about(request):
+  return render(request, 'shop/about.html') 
+
+def contact(request):
+  return render(request, 'shop/about.html')
+
+def tracker(request):
+  return HttpResponse("WE ARE AT TRACKER")    
+
+def search(request):
+  return HttpResponse("WE ARE AT Search")
+
+def prodView(request):
+  return HttpResponse("WE ARE AT Product View") 
+
+def checkout(request):
+  return HttpResponse("WE ARE AT CHECKOUT")     
